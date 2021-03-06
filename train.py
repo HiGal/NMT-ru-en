5fbf274d74e5049e4e3a1a23223f7025e71fb346
farit <f.galeev@innopolis.ru>
2021-03-06 19:42:34 +0300

    !modified training

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, Seq2SeqTrainer
 from transformers import Trainer, TrainingArguments
 from utils.dataset import YandexRuEnDataset, collate_sentences
 from models.marianmt import get_marianmt_model, get_marianmt_tokenizer
 
 if __name__ == '__main__':
     tokenizer = get_marianmt_tokenizer()
+    tokenizer.model_max_length=100
     model = get_marianmt_model()
     train_dataset = YandexRuEnDataset("data", split="train")
     val_dataset = YandexRuEnDataset("data", split="valid")
@@ -15,13 +16,15 @@ if __name__ == '__main__':
         num_train_epochs=3,  # total # of training epochs
         per_device_train_batch_size=16,  # batch size per device during training
         per_device_eval_batch_size=64,  # batch size for evaluation
-        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        warmup_steps=5000,  # number of warmup steps for learning rate scheduler
         weight_decay=0.01,  # strength of weight decay
         logging_dir='./logs',  # directory for storing logs
-
+        fp16=True,
+        fp16_opt_level='O2',
+        save_steps=20000
     )
 
-    trainer = Trainer(
+    trainer = Seq2SeqTrainer(
         model=model,  # the instantiated 🤗 Transformers model to be trained
         args=training_args,  # training arguments, defined above
         train_dataset=train_dataset,  # training dataset
