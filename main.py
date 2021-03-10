7bf82de2a1ec96b859c16838b58f3e6080d49224
Farit Galeev <f.galeev@innopolis.ru>
2021-03-10 19:41:10 +0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import unicodedata
-
-text = open("answer.txt", "r").read()
-open("n_answer.txt", "wb").write(unicodedata.normalize('NFKD', text).encode('ascii','ignore'))
-print(unicodedata.normalize('NFKD', text).encode('ascii','ignore'))
